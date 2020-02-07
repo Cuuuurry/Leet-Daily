c238d62bcaa7bad752453f5810aac68653e64fbb
Cuuuurry <scywu@ucdavis.edu>
2020-02-06 16:49:41 -0800

    daily practice

--- 215 Kth Largest Element in an Array/2-line heap method.py
+++ 215 Kth Largest Element in an Array/2-line heap method.py
@@ -4,6 +4,12 @@ import heapq
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        """
+        find the Kth largest number in a queue
+        :param nums:
+        :param k:
+        :return:
+        """
         heap = heapq.nlargest(k, nums)
         heapq.heapify(heap)
         return heapq.heappop(heap)
